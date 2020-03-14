eefe17bf4310908899c8303abfda33d078394005
caothuthao <thaoctt0809@gmail.com>
2020-03-14 22:27:08 +0700

    Completed assignment 4

---
+++ Java/Assignment 4/Sedan.java
@@ -0,0 +1,20 @@
+package asignment4;
+
+public class Sedan extends Car{
+	int length;
+	
+	Sedan (int speed, double regularPrice, String color, int length){
+		super(speed, regularPrice, color);
+		this.length = length;
+	}
+	
+	@Override
+	double getSalePrice(){
+		if (length >20){
+			return regularPrice*0.95;
+		} else {
+			return regularPrice*0.9;
+		}
+	}
+
+}
